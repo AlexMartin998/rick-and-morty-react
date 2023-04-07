0dd1af404bde4471dedc26335bd20ddbeb84aa93
Alex Martin <alextravc.342@gmail.com>
2023-04-07 08:08:20 -0500

    [ADD]: search icon responsive

--- src/shared/components/ui/Navbar/Navbar.tsx
+++ src/shared/components/ui/Navbar/Navbar.tsx
@@ -1,13 +1,29 @@
-import { AppBar, Box, Link, Toolbar, Typography } from '@mui/material';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Input,
+  InputAdornment,
+  Link,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { useMediaQuery } from '@mui/material';
+import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
+import { useUiStore } from '@/redux';
 import { CustomNavLink } from './components';
-import { navLinks } from './utils';
+import { navLinks } from '@/shared/utils';
 
 export interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
+  const { toggleMenu } = useUiStore();
+  const isMobile = useMediaQuery('(max-width: 600px)');
   const [isSearchVisible, setIsSearchVisible] = useState(false);
 
   return (
@@ -38,7 +54,46 @@ const Navbar: React.FC<NavbarProps> = () => {
 
         <Box flex={1} />
 
-        
+        {!isMobile && (
+          <>
+            {isSearchVisible ? (
+              <Input
+                autoFocus
+                type="text"
+                placeholder="Buscar..."
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setIsSearchVisible(false)}
+                    >
+                      <ClearOutlinedIcon />
+                    </IconButton>
+                  </InputAdornment>
+                }
+                className="fadeIn"
+                //
+                // value={searchTerm}
+                // onChange={e => setSearchTerm(e.target.value)}
+                // onKeyUp={e => e.key === 'Enter' && onSearchTerm()}
+                sx={{ zIndex: '999' }}
+              />
+            ) : (
+              <IconButton onClick={() => setIsSearchVisible(true)}>
+                <SearchOutlinedIcon />
+              </IconButton>
+            )}
+          </>
+        )}
+
+        {/* mobile */}
+        {isMobile && (
+          <IconButton onClick={toggleMenu}>
+            <SearchOutlinedIcon />
+          </IconButton>
+        )}
+
+        <Button onClick={toggleMenu}>Menu</Button>
       </Toolbar>
     </AppBar>
   );
--- src/shared/components/ui/SideMenu/SideMenu.tsx
+++ src/shared/components/ui/SideMenu/SideMenu.tsx
@@ -1,8 +1,18 @@
-import { Box, Drawer, List, useMediaQuery } from '@mui/material';
+import {
+  Box,
+  Drawer,
+  IconButton,
+  Input,
+  InputAdornment,
+  List,
+  ListItem,
+  useMediaQuery,
+} from '@mui/material';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
-import { NavLinkList } from './components';
-import { navLinks } from '../Navbar/utils/navLinks';
 import { useUiStore } from '@/redux';
+import { NavLinkList } from './components';
+import { navLinks } from '@/shared/utils';
 
 export interface SideMenuProps {}
 
@@ -18,6 +28,28 @@ const SideMenu: React.FC<SideMenuProps> = () => {
       sx={{ backdropFilter: 'blur(4px)', transition: 'all 0.5s ease-out' }}
     >
       <Box sx={{ width: 250, paddingTop: 5 }}>
+        <ListItem>
+          <Input
+            autoFocus
+            type="text"
+            placeholder="Search..."
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  // onClick={onSearchTerm}
+                >
+                  <SearchOutlinedIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+            //
+            // value={searchTerm}
+            // onChange={e => setSearchTerm(e.target.value)}
+            // onKeyUp={e => e.key === 'Enter' && onSearchTerm()}
+          />
+        </ListItem>
+
         <List>
           {isMobile &&
             navLinks.map(navLink => (
